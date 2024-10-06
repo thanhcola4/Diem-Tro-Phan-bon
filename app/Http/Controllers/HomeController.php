a845c72f758296a45640716558bfbe560535d5d8
thanhcola4 <thanhcola4@gmail.com>
2024-10-06 12:46:58 +0700

    dm o thay

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers;
 use App\Models\Contract;
-use App\Models\Services;
-
+use App\Models\Review;
+use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home()
     {
-       // Lấy danh sách đơn hàng mới nhất
-
-    // Lấy danh sách yêu cầu hỗ trợ từ khách hàng mới nhất
-    $supportRequests = Contract::latest()->take(5)->get();
-
-    // Lấy sản phẩm hoặc dịch vụ mới nhất
-    $recentServices = Services::latest()->take(5)->get();
-
-    // Trả về view dashboard với các biến đã được truyền vào
-    return view('dashboard', compact( 'supportRequests', 'recentServices'));
+    $TotalContacts = Contract::count(); 
+    $TotalReviews = Review::count();
+    $TotalOrders = Contact::count();
+    $TotalUsers = User::count();
+    return view('dashboard', compact( 'TotalContacts','TotalReviews','TotalOrders','TotalUsers'));
     }
+    
 }
--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -11,7 +11,7 @@
   <div class="collapse navbar-collapse  w-auto" id="sidenav-collapse-main">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <!-- <a class="nav-link {{ (Request::is('/') ? 'active' : '') }}" href="{{ url('/') }}">
+        <a class="nav-link {{ (Request::is('/') ? 'active' : '') }}" href="{{ url('/') }}">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <title>shop </title>
@@ -28,7 +28,7 @@
             </svg>
           </div>
           <span class="nav-link-text ms-1">Dashboard</span>
-        </a> -->
+        </a>
       </li>
       <li class="nav-item mt-2">
         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Nhân viên</h6>
